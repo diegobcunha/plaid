842b0f1398b9ab23c2172fa3b1f52e0f23a8f4e0
Diego Cunha <diego.cunha@warrenbrasil.com.br>
2021-03-03 20:10:32 -0300

    refactor :: Created module to enjoei features

---
+++ enjoeimodule/src/main/java/io/plaidapp/enjoeimodule/view/list/EnjoeiActivity.kt
@@ -0,0 +1,12 @@
+package io.plaidapp.enjoeimodule.view.list
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class EnjoeiActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+    }
+}
